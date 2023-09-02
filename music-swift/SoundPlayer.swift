3f0992aa98233cc31ebaaa8fc70b09dc537b5b17
kondo takeshi <chaspy@users.noreply.github.com>
2023-09-02 20:46:22 +0900

    feat: Add guitarPlay method to SoundPlayer

--- music-swift/SoundPlayer.swift
+++ music-swift/SoundPlayer.swift
@@ -11,6 +11,7 @@ import AVFoundation
 class SoundPlayer: NSObject {
     let cymbalData = NSDataAsset(name: "cymbalSound")!.data
     var cymbalPlayer: AVAudioPlayer!
+    
     func cymbalPlay() {
         do {
             cymbalPlayer = try AVAudioPlayer(data: cymbalData)
@@ -18,6 +19,19 @@ class SoundPlayer: NSObject {
             cymbalPlayer.play()
         } catch {
             print("シンバルで、エラーが発生しました！")
+        }
+    }
+    
+    let guitarData = NSDataAsset(name: "guitarSound")!.data
+    var guitarPlayer: AVAudioPlayer!
+    
+    func guitarPlay() {
+        do {
+            guitarPlayer = try AVAudioPlayer(data: guitarData)
+            
+            guitarPlayer.play()
+        } catch {
+            print("ギターで、エラーが発生しました！")
         }
     }
 }
