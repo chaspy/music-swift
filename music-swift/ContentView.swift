c23d72652f8337155ecf8674c6ab0963c6d22eea
kondo takeshi <chaspy@users.noreply.github.com>
2023-09-02 20:27:44 +0900

    init

---
+++ music-swift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  music-swift
+//
+//  Created by Takeshi Kondo on 2023/09/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ music-swift/music_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_swiftApp.swift
+//  music-swift
+//
+//  Created by Takeshi Kondo on 2023/09/02.
+//
+
+import SwiftUI
+
+@main
+struct music_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
